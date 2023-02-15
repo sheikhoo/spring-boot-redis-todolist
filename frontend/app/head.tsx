3327062f58d75ad6921b46ae25f790a55eb80b8b
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-02-15 11:48:48 +0330

    add nextjs frontend

---
+++ frontend/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Todo APP</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="todo app with springboot and nextjs" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="text-black bg-white dark:text-white dark:bg-gray-800 transition-colors duration-300">
+        {children}
+      </body>
+    </html>
+  );
+}
